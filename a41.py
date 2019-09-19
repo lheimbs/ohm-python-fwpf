5950afc7ac71182d33e8f04125f1d803b7d52c4b
Lennart Heimbs <l.heimbs@gmail.com>
2019-09-19 14:02:49 +0200

    Add a41

--- a41.py
+++ a41.py
@@ -1,2 +1,36 @@
 #!/usr/bin/env python
 
+import time, sys, os, argparse
+import tb_esp_lin as espCtl
+from datetime import datetime
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--temp", type=int, default=25,
+                help="Temperatur Schwellwert für Anschalten der LED")
+ap.add_argument("-f", "--file", default="temp", help="Locaiton of Temperatur-File.")
+FILE = ap.parse_args().file
+TEMP = ap.parse_args().temp
+
+start_time = time.time()
+wait_time = 30.0
+
+esp = espCtl.open_esp()
+if not esp:
+    print("Could not connect to esp!")
+    exit 1
+
+while True:
+    curr_temp = espCtl.query_txt(esp, 'md')
+    if curr_temp > TEMP:
+        espCtl.write_txt(esp, '1')
+    else:
+        espCtl.write_txt(esp, '0')
+
+    with open(FILE, "w") as f:
+        f.write(f"Time: {datetime.now()} Temperature: {curr_temp}\n")
+
+    now_time = time.time()
+    if now_time - start_time > 60.0*5:
+        break
+    else:
+        time.sleep(wait_time - ((now_time - start_time) % wait_time))
--- a42.py
+++ a42.py
@@ -8,7 +8,10 @@ def H(f):
     R = 1590
     return 1 / (1 + 1j * 2*np.pi * f * C * R)
 
-x = np.logspace(1, 6, num=50, dtype=complex)
+x = np.logspace(1, 7, num=50, dtype=complex)
 
+plt.subplot(211)
 plt.plot(x, H(x))
+plt.subplot(212)
+plt.plot(x, 20*np.log10(abs(H(x))))
 plt.show()
